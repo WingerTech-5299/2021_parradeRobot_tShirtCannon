04c48ba7cc3a18bed01d99616396b1f4aea7e33a
Bryce Engberg <bryceengberg@gmail.com>
2021-05-24 12:28:39 -0500

    bink

--- tShirtCannon/src/main/java/frc/robot/Robot.java
+++ tShirtCannon/src/main/java/frc/robot/Robot.java
@@ -11,8 +11,11 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -50,13 +53,30 @@ public class Robot extends TimedRobot {
   Double btn_TurretRotateZ = xbox.getRawAxis(0);
   Double btn_TurretRotateY = xbox.getRawAxis(1);
   Boolean btn_CannonFire = xbox.getRawButton(0);
+  Boolean btn_ResetLoaded = xbox.getRawButton(1);
 
   //Pneumatics
   Compressor compressor = new Compressor(0);
-  
+
   Solenoid sol_fire1 = new Solenoid(0);
   Solenoid sol_fire2 = new Solenoid(1);
 
+  //Sensors
+  AnalogPotentiometer barrel1Sensor = new AnalogPotentiometer(0);
+  AnalogPotentiometer barrel2Sensor = new AnalogPotentiometer(1);
+
+  // Sensor Variables
+  Double barrel1Distance = barrel1Sensor.get()*5;
+  Double barrel2Distance = barrel2Sensor.get()*5;
+
+  //Counting variables
+  Boolean cannonPrimed;
+  Boolean barrel1loaded;
+  Boolean barrel2loaded;
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
   @Override
   public void robotInit() {}
 
@@ -80,8 +100,53 @@ public class Robot extends TimedRobot {
     btn_TurretRotateZ = xbox.getRawAxis(0);
     btn_TurretRotateZ = xbox.getRawAxis(1);
     btn_CannonFire = xbox.getRawButton(0);
+    btn_ResetLoaded = xbox.getRawButton(1);
+
+    if (barrel1Distance < 20){
+      barrel1loaded = true;
+    } else {
+      barrel1loaded = false;
+    }
+    if (barrel2Distance < 20){
+      barrel2loaded = true;
+    } else {
+      barrel2loaded = false;
+    }
+
+    if (cannonPrimed){
+      if (btn_CannonFire){
+        if (barrel1loaded){
+
+          sol_fire1.set(true);
+          Timer.delay(0.2);
+          sol_fire1.set(false);
+
+          barrel1loaded = false;
+
+        } else if (barrel2loaded){
+
+          sol_fire2.set(true);
+          Timer.delay(0.2);
+          sol_fire2.set(false);
+
+        } else {
+          xbox.setRumble(RumbleType.kRightRumble, 1);
+          xbox.setRumble(RumbleType.kLeftRumble, 1);
+
+          Timer.delay(0.4);
+
+          xbox.setRumble(RumbleType.kLeftRumble, 0);
+          xbox.setRumble(RumbleType.kRightRumble, 0);
+        }
+      }
+    }
 
 
+
+
+    //Drivetrain stuff
+  
+    drive.arcadeDrive(0.8 * btn_DriveFB, 0.8 * btn_DriveSpin);
 
   }
 
